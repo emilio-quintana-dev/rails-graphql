6e12c5a75baaab84fed581f3de2e9c009ee85fd1
Emilio Quintana <emilioquintana90@gmail.com>
2024-01-28 20:46:11 +0800

    Generate base graphql types for each model

---
+++ app/graphql/types/modifier_group_type.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Types
+  class ModifierGroupType < Types::BaseObject
+    field :id, ID, null: false
+    field :identifier, String
+    field :label, String
+    field :selection_required_min, Integer
+    field :selection_required_max, Integer
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
---
+++ app/graphql/types/modifier_type.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Types
+  class ModifierType < Types::BaseObject
+    field :id, ID, null: false
+    field :item_id, Integer, null: false
+    field :display_order, Integer
+    field :default_quantity, Integer
+    field :price_override, Float
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end

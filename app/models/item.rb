1a43157fb872cff924e12dfce22683b6a4286d73
Emilio Quintana <emilioquintana90@gmail.com>
2024-01-29 11:27:32 +0800

    Add more model-level validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   validates :identifier, presence: true, uniqueness: true
+  validates :type, presence: true
+  validates :label, presence: true
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,5 @@
 class Menu < ApplicationRecord
   validates :identifier, presence: true, uniqueness: true
+  validates :label, presence: true
+  validates :state, presence: true
 end

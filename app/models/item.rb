5eca3df7ee41a8adaa876c663781f49edbca049a
Emilio Quintana <emilioquintana90@gmail.com>
2024-01-29 12:55:49 +0800

    Add workaround for STI Rails issue with the type field

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   validates :identifier, presence: true, uniqueness: true
   validates :type, presence: true
   validates :label, presence: true
+
+  # [NOTE]: This is a workaround for the STI implementation in Rails
+  self.inheritance_column = 'sti_type'
 end

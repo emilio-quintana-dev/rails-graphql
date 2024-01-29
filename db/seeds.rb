ce80e534b92dfe4b0fc347bf29da87946b3138cb
Emilio Quintana <emilioquintana90@gmail.com>
2024-01-29 12:56:12 +0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+menu = Menu.create!(
+  identifier: 'pizza-menu',
+  label: 'Pizza Menu',
+  state: 'active',
+  start_date: 1.year.ago,
+  end_date: 1.year.from_now
+)
+section = Section.create!(
+  identifier: 'classic-pizzas',
+  label: 'Classic Pizzas',
+  description: 'Our classic pizzas are made with our signature sauce and a blend of mozzarella and provolone cheeses.',
+  display_order: 0
+)
+menu_section = MenuSection.create!(
+  menu:,
+  section:
+)
+item = Item.create!(
+  identifier: 'margarita-pizza',
+  label: 'Margarita Pizza',
+  description: 'Our classic margarita pizza is made with our signature sauce, fresh mozzarella, and fresh basil.',
+  price: 19.99,
+  type: 'pizza'
+)
+# section_item = SectionItem.create!(
+#   section:,
+#   item:,
+#   display_order: 0
+# )
 #
-# Examples:
+# modifier_group = ModifierGroup.create!(
+#   identifier: 'pizza-size',
+#   label: '10"',
+#   selection_required_min: 1,
+#   selection_required_max: 1
+# )
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# modifier = Modifier.create!(
+#   modifier_group:,
+#   item:,
+#   display_order: 0,
+#   default_quantity: 0,
+#   price_override: nil
+# )
+# ItemModifierGroup.create!(
+#   item:,
+#   modifier_group:
+# )

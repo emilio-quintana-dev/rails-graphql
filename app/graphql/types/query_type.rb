8a36b754f20b7ed25a6b2b6e21844e73b8f88f8b
Emilio Quintana <emilioquintana90@gmail.com>
2024-01-31 14:44:55 +0800

    Add the equivalent of show routes for resources

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,30 +2,144 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
+    # Menu Endpoints
+
+    # Get all
+    field(
+      :menus,
+      [Types::MenuType],
+      null: true,
+      description: 'Endpoint to retrieve all menus'
+    )
+
+    def menus
+      Menu.all
     end
 
-    def node(id:)
-      context.schema.object_from_id(id, context)
+    # Get one (by ID)
+    field(
+      :menu,
+      Types::MenuType,
+      null: true,
+      description: 'Endpoint to retrieve a menu'
+    ) do
+      argument :id, ID, required: true, description: 'ID of the menu'
     end
 
-    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
+    def menu(id:)
+      Menu.find(id)
     end
 
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
+    # Section Endpoints
+
+    # Get all
+    field(
+      :sections,
+      [Types::SectionType],
+      null: true,
+      description: 'Endpoint to retrieve all sections'
+    )
+
+    def sections
+      Section.all
     end
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    # Get one (by ID)
+    field(
+      :section,
+      Types::SectionType,
+      null: true,
+      description: 'Endpoint to retrieve a section'
+    ) do
+      argument :id, ID, required: true, description: 'ID of the section'
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def section(id:)
+      Section.find(id)
+    end
+
+    # Items Endpoints
+
+    # Get all
+    field(
+      :items,
+      [Types::ItemType],
+      null: true,
+      description: 'Endpoint to retrieve all items'
+    )
+
+    def items
+      Item.all
+    end
+
+    # Get one (by ID)
+    field(
+      :item,
+      Types::ItemType,
+      null: true,
+      description: 'Endpoint to retrieve an item'
+    ) do
+      argument :id, ID, required: true, description: 'ID of the item'
+    end
+
+    def item(id:)
+      Item.find(id)
+    end
+
+    # Modifier Groups Endpoints
+
+    # Get all
+    field(
+      :modifier_groups,
+      [Types::ModifierGroupType],
+      null: true,
+      description: 'Endpoint to retrieve all modifier groups'
+    )
+
+    def modifier_groups
+      ModifierGroup.all
+    end
+
+    # Get one
+    field(
+      :modifier_group,
+      Types::ModifierGroupType,
+      null: true,
+      description: 'Endpoint to retrieve a modifier group'
+    ) do
+      argument :id, ID, required: true, description: 'ID of the modifier group'
+    end
+
+    def modifier_group(id:)
+      ModifierGroup.find(id)
+    end
+
+    # Modifiers Endpoints
+
+    # Get all
+    field(
+      :modifiers,
+      [Types::ModifierType],
+      null: true,
+      description: 'Endpoint to retrieve all modifiers'
+    )
+
+    def modifiers
+      Modifier.all
+    end
+
+    # Get one
+    field(
+      :modifier,
+      Types::ModifierType,
+      null: true,
+      description: 'Endpoint to retrieve a modifier'
+    ) do
+      argument :id, ID, required: true, description: 'ID of the modifier'
+    end
+
+    def modifier(id:)
+      Modifier.find(id)
     end
   end
 end

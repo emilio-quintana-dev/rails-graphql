964c29b5bb145e5bb68adc89ab0598607084ea00
Emilio Quintana <emilioquintana90@gmail.com>
2024-02-05 21:38:18 +0800

    Add rack cors gem and configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
 
-  post '/graphql', to: 'graphql#execute'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/', to: 'graphql#execute'
 end
